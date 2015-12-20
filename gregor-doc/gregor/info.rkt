494456391c8941b96f1746bcdcada189a607a3bb
Jon Zeppieri <jon.zeppieri@meyouhealth.com>
2015-12-19 22:02:31 -0500

    Moves docs to "Date, Time, and Calendar Libraries" category

--- gregor-doc/gregor/info.rkt
+++ gregor-doc/gregor/info.rkt
@@ -1,4 +1,4 @@
 #lang info
 
 (define collection "gregor")
-(define scribblings '(("scribblings/gregor.scrbl" (multi-page))))
+(define scribblings '(("scribblings/gregor.scrbl" (multi-page) ("Date, Time, and Calendar Libraries"))))
